d358922f26f526f64d6117fa3933ed48538a08d6
kaiovinic <kaiovini19@hotmail.com>
2024-08-14 22:28:39 -0300

    Criando pages e tipagens

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 
+import UserCreate from "./pages/Users/Create";
+import UserList from "./pages/Users/List";
+import UserEdit from "./pages/Users/Edit";
+
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/users">
-        <Route path="/users" element={<>Users</>} />
-        <Route path="/users/new" element={<>New Users</>} />
-        <Route path="/users/:id" element={<>Edit Users</>} />
+        <Route path="/users" element={<UserList />} />
+        <Route path="/users/new" element={<UserCreate />} />
+        <Route path="/users/:id" element={<UserEdit />} />
       </Route>
     </Routes>
   );
